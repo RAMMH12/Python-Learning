5bfca5bbd1dd5e7c183df15a5fc797336c50f6ec
RAMMH12 <ram5393@gmail.com>
2025-03-31 00:01:59 +0530

    Add files via upload

---
+++ evenoddNumber.py
@@ -0,0 +1,5 @@
+num1= int(input('Enter a number: '))
+if num1%2:
+    print(num1,' is an odd number.')
+else:
+    print(num1,' is an even number')
---
+++ sumOf50Number.py
@@ -0,0 +1,5 @@
+num = 0
+for i in range(51):
+    num = num+i
+
+print('Sum of numbers from 1 to 50 is : ',num)
